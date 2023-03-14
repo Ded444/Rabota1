1fffd69d0e441cedaf9e42ced5217906fa3d7d13
SergeyKruchkov <vvkvkua@mail.ru>
2023-03-14 21:16:52 +0300

    Lab4

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Lab4 {
+  static void Main() {
+   string input = "Я! не учусь в рмт!!!";
+    string[] words = input.Split(' ');
+    string longestWord = "";
+    foreach (string word in words)
+        {
+            if (word.Length > longestWord.Length)
+                {
+                    longestWord = word;
+                }
+        }           
+Console.WriteLine($"Самое длинное слово - '{longestWord}' включает в себя {longestWord.Length} символов.");
+    }
+}
