0175bbc4710849546bfdeafca07b81ee3091c4ed
Ded444 <123232594+Ded444@users.noreply.github.com>
2023-03-14 21:22:42 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,18 @@
-﻿using System;
-class Lab4 {
-  static void Main() {
-   string input = "Я! не учусь в рмт!!!";
-    string[] words = input.Split(' ');
-    string longestWord = "";
-    foreach (string word in words)
+using System;
+class Lab4
+{
+    static void Main()
+    {
+        string input = "Я! не учусь в рмт!!!";
+        string[] words = input.Split(' ');
+        string longestWord = "";
+        foreach (string word in words)
         {
             if (word.Length > longestWord.Length)
-                {
-                    longestWord = word;
-                }
-        }           
-Console.WriteLine($"Самое длинное слово - '{longestWord}' включает в себя {longestWord.Length} символов.");
+            {
+                longestWord = word;
+            }
+        }
+        Console.WriteLine($"Самое длинное слово - '{longestWord}' включает в себя {longestWord.Length} символов.");
     }
 }
